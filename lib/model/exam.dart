85b59a05efb56a01c486d3206c387b22a8fe01db
다정 <work.99.da@gmail.com>
2024-12-05 15:31:46 +0900

    페이지 통합

--- lib/model/exam.dart
+++ lib/model/exam.dart
@@ -57,7 +57,6 @@ class _QuestionScreenState extends State<Exam> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
